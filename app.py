7c944278ff2e3b828aafeaa0e1b5bec4892ca312
Difa Habiba Rahman <difa.habiba.rahman@gmail.com>
2019-11-03 06:04:24 +0700

    Update app.py (Cosine Similarity)

--- app.py
+++ app.py
@@ -6,3 +6,23 @@ def Euclidean (v,w):
         c += (v[i] - w[i]) ** 2
     d = c ** (1/2)
     return d
+# Dot Product
+def DotVectors(a, b):
+    # a dan b memiliki banyak komponen sama, tidak nol
+    n = len(a)
+    c = 0
+    for i in range(n):
+        c = c + (a[i] * b[i])
+    return c
+# Panjang Vektor
+def VectorLength(a):
+    # a tidak nol
+    c = 0
+    n = len(a)
+    for i in range(n):
+        c = (a[i]**2) + c
+    d = c ** 0.5
+    return d
+# Cosine Similarity
+def CosSim(a, b):
+    return DotVectors(a, b)/(VectorLength(a)*VectorLength(b))
